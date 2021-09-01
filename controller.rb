bef1296e2cb90c43f1705460a6c0e2ac7b7c4036
Markus-Moen <moenm@student.chalmers.se>
2021-09-01 16:51:26 +0200

    Now gives errors if a problematic number of cities are found.

--- controller.rb
+++ controller.rb
@@ -9,6 +9,39 @@ include Model
 # Displays the landing page
 #
 get('/') do
-    city_search()
-    slim(:home)
+    slim(:index)
+end
+
+get("/error/:query") do
+    data = params["query"].split(',')
+    slim(:index, locals:{vars:data})
+end
+
+# Manages searches. Either sends to search results, to a selection page if there were many results, 
+# or back to '/' if search was too unclear
+#
+# @param [string] City, The search entered by a user
+#
+# @see Model#city_search
+post('/search') do
+    max_search_results = 15
+    search = params["City"]
+    cities = city_search(search)
+    len = cities.length
+    if len == 1
+        #search results directly
+    elsif 0 < len && len <= max_search_results
+        #redirect to a selection of results
+    else
+        #return to search with error saying if it's too much or too little.
+        query = "#{max_search_results},"
+        if len == 0
+            query += "noResult"
+        elsif len > max_search_results
+            query += "overflow"
+        else
+        end
+        redirect("/error/#{query}")
+    end
+    redirect("/")
 end
--- model.rb
+++ model.rb
@@ -1,21 +1,36 @@
 require 'json'
 
 module Model
-    def city_search()
-        # cityFile = ""
-        prevLine = ""
+    # Searches for all cities matching a search word
+    #
+    # @param [String] search is the searchword entered to look for a city
+    #
+    # @return [Hash]
+    #   * IDs [String] The IDs act as keys for their respective cities
+    def city_search(search)
+        if search == ""
+            return {}
+        end
         names = []
         ids = []
+        prevLine = ""
         File.foreach("json/city.list.json") do |line|
-            #cityFile += line
-            if line.downcase.include? "gerton"
+            if line.downcase.force_encoding(::Encoding::UTF_8).include? search.downcase.force_encoding(::Encoding::UTF_8)
                 names << line
                 ids << prevLine
             end
             prevLine = line
         end
-        puts id_list(names, ids)
+        return id_list(names, ids)
     end
+
+    # Creates a dictionary of cities using their IDs as keys
+    #
+    # @param [Array] h1 is an array containing the lines displaying the city names in Openweathermaps list of city IDs
+    # @param [Array] h2 is an array containing the lines displaying the city IDs in Openweathermaps list of city IDs
+    #
+    # @return [Hash]
+    #   * IDs [String] The IDs act as keys for their respective cities
     def id_list(h1, h2)
         output = {}
         names = []
@@ -26,7 +41,7 @@ module Model
         end
         h2.each do |line|
             arr = line.delete_suffix(",\n").split(' ')
-            ids << arr[-1]
+            ids << arr[-1].to_i
         end
         for i in 0..(names.length-1) do
             output[ids[i]] = names[i]
